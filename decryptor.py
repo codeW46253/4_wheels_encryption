f37b97cecdb47bced3f91b51f1f4ac35963ce748
CodeW46253 <73611813+codeW46253@users.noreply.github.com>
2023-03-01 23:15:29 +0800

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -1,14 +1,29 @@
+
 import wheel_setup
 
 ################################################################################################
-
+# Decryption
+#
+# See step and order for setting wheel on "Setting.txt" file
+#
+# Decrypt any text with this step:
+#   1) call "Decryptor" class
+#   2) set the wheels selection acording to this list:
+#       0 - wheel_setup.wheel_1
+#       1 - wheel_setup.wheel_2
+#       2 - wheel_setup.wheel_3
+#       3 - wheel_setup.wheel_4
+#   
+#   3) enter 3 level shift key [main lock for encryption] only arround 0<= x <= lenght
+#      of main character's arrangement
+#   4) call "decryption" function from "Decryptor" class and push your code, shift key
+#      and special key [produced by encryption of text/code]
+#
+################################################################################################
 
 
 ################################################################################################
-
 class Decryptor:
-    
-# initial value & placeholder
     def __init__(self):
         self.encr_w1 = []
         self.encr_w2 = []
@@ -20,9 +35,10 @@ class Decryptor:
         self.encr_key2 = 0
         self.encr_key3 = 0
         
+    # step setting for encryption and decryption [Don't edit unless you know how to handle it]
     def order_setting(self):
         self.order = [[self.alpha_list, self.encr_w1, self.encr_w3, self.encr_w3, self.encr_w1], # from wheel
-                      [self.encr_w1, self.encr_w2, self.encr_w4, self.encr_w2, self.alpha_list], # to_wheel
+                      [self.encr_w1, self.encr_w2, self.encr_w4, self.encr_w2, self.alpha_list], # to wheel
                       [0b10000, 0b01000, 0b00100, 0b00010, 0b00001], # lock
                       [self.encr_key1, self.encr_key2, self.encr_key3, self.encr_key2, self.encr_key1] # key
         ]
@@ -30,10 +46,8 @@ class Decryptor:
         self.order_lenght = 5
             
 ################################################################################################
-
 # settings : 1) Wheel
 #            2) Shift key
-            
     def wheel_setting(self, w1, w2, w3, w4):
         wheels = [wheel_setup.wheel_1,
                   wheel_setup.wheel_2,
@@ -46,15 +60,15 @@ class Decryptor:
         self.encr_w4 = wheels[w4]
         
     def key_setting(self, key1, key2, key3):
-        self.encr_key1 = key1 % 69
-        self.encr_key2 = key2 % 69
-        self.encr_key3 = key3 % 69
-   
-################################################################################################
+        self.encr_key1 = key1 % wheel_setup.alpha_len
+        self.encr_key2 = key2 % wheel_setup.alpha_len
+        self.encr_key3 = key3 % wheel_setup.alpha_len
 
-# system for character/alphabet swap
-    
+################################################################################################
+# code for character/alphabet swap
     def rechange(self, alpha_from_wheel, alpha_to_wheel, code, shift_key, lock, comb):
+        """rechange(wheel for character input referencing, wheel for character output
+        referencing, code, shift key, additional lock, combination [from order_setting]"""
         index_of_item = alpha_from_wheel.index(code)
         new_index = index_of_item
         
@@ -69,9 +83,10 @@ class Decryptor:
         return alpha_to_wheel[new_index]
      
 ################################################################################################
-        
-# system for decryption
+# code for decryption
     def decryption(self, code, lock):
+        """decryption(code, additional lock)"""
+        self.order_setting()
         code_parting = []
         for i in range(len(code)):
             if code[i] != "\n" or code[i] != "\t":
@@ -101,11 +116,11 @@ class Decryptor:
             
             # incrementer
             self.encr_key1 += 1
-            if self.encr_key1 > 69:
+            if self.encr_key1 > wheel_setup.alpha_len:
                 self.encr_key2 += 1
-                if self.encr_key2 > 69:
+                if self.encr_key2 > wheel_setup.alpha_len:
                     self.encr_key3 += 1
-                    if self.encr_key3 > 69:
+                    if self.encr_key3 > wheel_setup.alpha_len:
                         self.encr_key3 = 0
                         
                     self.encr_key2 = 0
@@ -117,15 +132,13 @@ class Decryptor:
         return [decrypted]
         
 ################################################################################################
-
 if __name__=="__main__":
     system_selected = Decryptor()
     system_selected.wheel_setting(0,1,2,3)
     system_selected.key_setting(0, 0, 0)
-    system_selected.order_setting()
     
-    decrypted = system_selected.decryption("99RIB>\'\"2W",
-                                           [0, 0, 0, 0, 16, 1, 0, 0, 0, 0])
+    decrypted = system_selected.decryption("",
+                                           [])
     
     print(decrypted)
 
